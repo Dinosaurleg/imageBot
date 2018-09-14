01a83df3c5ca0ef671869dac61d80f6425f5dd78
dinosaurleg <j.shin5553@gmail.com>
2018-09-14 13:11:40 -0500

    Changed file name due to conflict with discord lib and added pycache to gitignore

---
+++ imageBot.py
@@ -0,0 +1,30 @@
+# Python 3
+# test project for image/link posting bot
+
+import discord
+import json
+
+with open('config.json', 'r') as f:
+	config = json.load(f)
+
+BOT_TOKEN = config['DISCORD_BOT']['TOKEN']
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+
+	if message.content.startswith('hello'):
+		msg = 'Hello {0.author.mention'.format(message)
+		await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+	print('Logged in as: ')
+	print(client.user.name)
+	print(client.user.id)
+	print('--------------')
+
+client.run(BOT_TOKEN)
